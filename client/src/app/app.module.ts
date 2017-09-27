7498de7d5ec6b36fef7e525329ca58d93e82d511
Jason Walsh <jwalsh@saylent.com>
2017-09-27 18:16:24 -0400

    Scaffold login

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NotificationsComponent } from './notifications/notifications.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotificationsComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent
   ],
-  imports: [BrowserModule, HttpModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
