926c4bd144f1ee53cfa7c5a815efe0879c24f6c9
Jason Walsh <jwalsh@saylent.com>
2017-09-27 17:07:20 -0400

    header, footer, and notifications components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotificationsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
